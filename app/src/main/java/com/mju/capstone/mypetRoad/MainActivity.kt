3f527cb4cec8c911ce266ce5227d23be474a383d
almond0115 <jjh19960115@gmail.com>
2023-04-01 21:05:22 +0900

    Solved R problem

--- app/src/main/java/com/mju/capstone/mypetRoad/MainActivity.kt
+++ app/src/main/java/com/mju/capstone/mypetRoad/MainActivity.kt
@@ -2,6 +2,7 @@ package com.mju.capstone.mypetRoad
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mju.capstone.mypetRoad.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
