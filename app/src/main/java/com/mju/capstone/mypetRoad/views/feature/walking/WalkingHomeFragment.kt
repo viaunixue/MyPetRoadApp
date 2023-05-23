d0cb59fb6886bba2882bb9a4ec47b97186cddf00
almond0115 <jjh19960115@gmail.com>
2023-05-24 01:33:38 +0900

    Updated Card View Logic

--- app/src/main/java/com/mju/capstone/mypetRoad/views/feature/walking/WalkingHomeFragment.kt
+++ app/src/main/java/com/mju/capstone/mypetRoad/views/feature/walking/WalkingHomeFragment.kt
@@ -11,12 +11,15 @@ import androidx.annotation.UiThread
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.fragment.NavHostFragment
+import androidx.viewpager2.widget.ViewPager2
 import com.mju.capstone.mypetRoad.R
 import com.mju.capstone.mypetRoad.data.retrofit.RetrofitManager
 import com.mju.capstone.mypetRoad.databinding.FragmentWalkingHomeBinding
+import com.mju.capstone.mypetRoad.domain.model.WalkingLog
 import com.mju.capstone.mypetRoad.util.Config
 import com.mju.capstone.mypetRoad.views.MainActivity
 import com.mju.capstone.mypetRoad.views.base.BaseFragment
+import com.mju.capstone.mypetRoad.widget.Adapter.WalkingLogAdapter
 import com.naver.maps.map.MapView
 import com.naver.maps.map.NaverMap
 import com.naver.maps.map.OnMapReadyCallback
@@ -61,6 +64,34 @@ class WalkingHomeFragment : BaseFragment<FragmentWalkingHomeBinding>(), OnMapRea
 
     override fun initViews() {
         super.initViews()
+
+        val viewPager = binding.walkingLogViewPager
+        var walkingLog: List<WalkingLog> = listOf(
+            WalkingLog(R.drawable.sample_map_view, "2023/05/17", "수요일 오후 산책", 3.67, "10'55\"", "12:08"),
+            WalkingLog(R.drawable.sample_map_view, "2022/04/20", "화요일 오전 산책", 2.67, "20'44\"", "10:08"),
+            WalkingLog(R.drawable.sample_map_view, "2021/03/23", "월요일 새벽 산책", 1.67, "30'33\"", "08:08"),
+            WalkingLog(R.drawable.sample_map_view, "2020/02/26", "일요일 오후 산책", 0.67, "40'22\"", "06:08"),
+            WalkingLog(R.drawable.sample_map_view, "2019/01/29", "토요일 오전 산책", 0.07, "50'11\"", "04:08")
+        )
+
+        var walkingLogadapter = WalkingLogAdapter(walkingLog)
+        viewPager.adapter = walkingLogadapter
+        viewPager.setPadding(30, 0, 30, 0)
+
+        viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+                super.onPageScrolled(position, positionOffset, positionOffsetPixels)
+            }
+
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+            }
+
+            override fun onPageScrollStateChanged(state: Int) {
+                super.onPageScrollStateChanged(state)
+            }
+        })
+
         binding.btnWalkingStart.setOnClickListener {
             Config.isWalking = true
             view?.let { walkingMode ->
